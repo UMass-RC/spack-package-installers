fbab2429ca920f24c2501c55ff58baee8300a238
hakasapl <hakansaplakog@gmail.com>
2022-04-21 06:22:04 +0000

    added deployment scripts

---
+++ slurm-install-batch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -c 8  # Number of Cores per Task
+#SBATCH -t 12:00:00  # Job time limit
+#SBATCH -p building
+
+echo "Loading spack environment..."
+source /modules/spack/share/spack/setup-env.sh
+
+echo "Installing spack package ${SPACK_PACKAGE_NAME}..."
+spack install -y $SPACK_PACKAGE_NAME
