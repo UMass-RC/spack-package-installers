bc156699bbd40094b93cb573feee6067f1e60d0b
simon <simon.leary42@gmail.com>
2022-08-19 00:14:01 -0400

    update gpu versions

--- install-package-gpu.sh
+++ install-package-gpu.sh
@@ -3,23 +3,54 @@
 # USAGE
 # ./install-package <spack package spec>
 
-#SPACK_PACKAGE_NAME=$1
-SPACK_PACKAGE_NAME=$*
-JOB_NAME="build_${SPACK_PACKAGE_NAME// /_}" # find and replace spaces with underscores
+# despite that this modulepath only includes x86_64,
+# it seems to update the cache for other microarches as well
+HARD_MODULEPATH="/modules/spack/share/spack/modules/linux-ubuntu20.04-x86_64:/modules/modulefiles"
 
-#echo "spack install $SPACK_PACKAGE_NAME"
-#[[ "$(read -e -p 'is this correct? [y/N]> '; echo $REPLY)" == [Yy]* ]] || exit
+SPACK_INSTALL_ARGS=$@
+JOB_NAME="${SPACK_INSTALL_ARGS// /_}" # find and replace spaces with underscores
+RANDOM_STR=$( echo $RANDOM | md5sum | head -c 5; echo;)
 
+echo "Loading spack environment..."
+source /modules/spack/share/spack/setup-env.sh
+echo
 
-IFS=$'\n' read -d '' -r -a lines < state/archlist.txt
-
-for i in "${lines[@]}"
-do
-    echo "Queuing job for architecture $i..."
-    sbatch --job-name="$JOB_NAME" --constraint=$i --output=logs/$JOB_NAME-$i.out \
-	    --export=SPACK_PACKAGE_NAME="$SPACK_PACKAGE_NAME" slurm/slurm-install-batch-gpu.sh
-    echo log file: logs/$JOB_NAME-$i.out
+NUM_JOBS=0
+for arch in $(<state/archlist.txt); do
+    LOG_FILE="logs/${JOB_NAME}_${arch}_${RANDOM_STR}.out" # random so that logs don't overwrite
+    echo "install #$(( $NUM_JOBS+1 )): $LOG_FILE"
+    sbatch --wait --job-name="build_$JOB_NAME" --constraint=$arch --output=$LOG_FILE \
+            --export=SPACK_INSTALL_ARGS="$SPACK_INSTALL_ARGS" slurm/slurm-install-batch-gpu.sh\
+            & # & means run this in the background
+    ((NUM_JOBS++))
 done
 
-# add to package file if not already there
-grep -qxF "$SPACK_PACKAGE_NAME" state/packagelist.txt || echo $SPACK_PACKAGE_NAME >> state/packagelist.txt
+echo
+echo "this might take a while. You can break out of this script and the installs will continue,"
+echo "but you will have to check by hand that the installs were successful."
+echo
+
+ANY_FAILURES=0
+for ((i=1; i<($NUM_JOBS+1); i++)); do
+    # wait %i -> get the return code for background process i
+    # multiple waits called in sequence will run in parallel
+    # the background processes are indexed starting at 1
+    # if a job is held up in the queue then the `wait`s after it won't start until it gets out
+    wait %$i
+    if [ ! $? -eq 0 ]; then
+        echo "install #$i has failed!"
+        ANY_FAILURES=1
+    fi
+done
+
+if [ $ANY_FAILURES -eq 1 ]; then
+    exit 1
+fi
+
+grep -qxF "$SPACK_INSTALL_ARGS" state/packagelist.txt || echo $SPACK_INSTALL_ARGS >> state/packagelist.txt
+echo "regenerating Lmod spider cache using hard-coded modulepath. This may change someday!"
+echo $HARD_MODULEPATH
+# remove implicit (dependent) modules from `module av`
+./hide-implicit-mods.py
+# update the Lmod spider cache
+/modules/lmod/lmod/lmod/libexec/update_lmod_system_cache_files -d /modules/lmod/cache $HARD_MODULEPATH
--- slurm/slurm-install-batch-gpu.sh
+++ slurm/slurm-install-batch-gpu.sh
@@ -4,8 +4,17 @@
 #SBATCH -p building
 #SBATCH -G 1
 
+echo jobid $SLURM_JOB_ID on host $(hostname) with arch $(/modules/spack/bin/spack arch) by user $(whoami) on $(date)
+
 echo "Loading spack environment..."
 source /modules/spack/share/spack/setup-env.sh
 
 echo spack install --verbose -y $SPACK_INSTALL_ARGS
 spack install --verbose -y $SPACK_INSTALL_ARGS
+
+#--keep-stage will leave the build logs in /scratch, which will get cleaned up by /scratch anyways
+# but it only does so if the installation was a success, and that's exactly when I don't want the logs
+
+#--debug will say a lot of stuff and include tracebacks in spack codebase
+
+#-V will spit out much (but not all) of the build log in the regular log. Good because the build log gets deleted.
