b09aa2873b2d165f616ac107123933a32202713b
simon <simon.leary42@gmail.com>
2022-08-19 13:00:27 -0400

    add success message

--- install-package.sh
+++ install-package.sh
@@ -31,6 +31,7 @@ echo "but you will have to check by hand that the installs were successful, and 
 echo
 
 ANY_FAILURES=0
+ANY_SUCCESSES=0
 for ((i=1; i<($NUM_JOBS+1); i++)); do
     # wait %i -> get the return code for background process i
     # multiple waits called in sequence will run in parallel
@@ -39,21 +40,29 @@ for ((i=1; i<($NUM_JOBS+1); i++)); do
     wait %$i
     if [ $? -eq 0 ]; then
         echo "install #$i was a success!"
+        ANY_SUCCESSES=1
     else
         echo "install #$i has failed!"
         ANY_FAILURES=1
     fi
 done
 
+if [ $ANY_FAILURES -eq 1 ] & [ $ANY_SUCCESSES -eq 1 ]; then
+    echo "ACTION REQUIRED"
+    echo "some installs succeeded but some failed! You will have to clean this up by hand."
+fi
+
 if [ $ANY_FAILURES -eq 1 ]; then
     exit 1
 fi
 
 # post install cleanup
+# add the install to the packagelist
 grep -qxF "$SPACK_INSTALL_ARGS" state/packagelist.txt || echo $SPACK_INSTALL_ARGS >> state/packagelist.txt
+echo
+echo "removing implicit modules from Lmod"
+./hide-implicit-mods.py
+echo
 echo "regenerating Lmod spider cache using hard-coded modulepath. This may change someday!"
 echo $HARD_MODULEPATH
-# remove implicit (dependent) modules from `module av`
-./hide-implicit-mods.py
-# update the Lmod spider cache
 /modules/lmod/lmod/lmod/libexec/update_lmod_system_cache_files -d /modules/lmod/cache $HARD_MODULEPATH
