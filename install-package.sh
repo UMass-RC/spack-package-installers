394489839ddb4291d9e15cdc3004ef1abbc9774b
simon <simon.leary42@gmail.com>
2022-08-18 23:09:48 -0400

    rewrite install-package

--- install-package.sh
+++ install-package.sh
@@ -3,29 +3,46 @@
 # USAGE
 # ./install-package <spack package spec>
 
-#SPACK_PACKAGE_NAME=$1
-SPACK_PACKAGE_NAME=$*
-JOB_NAME="build_${SPACK_PACKAGE_NAME// /_}" # find and replace spaces with underscores
+# despite that this modulepath only includes x86_64,
+# it seems to update the cache for other microarches as well
+HARD_MODULEPATH="/modules/spack/share/spack/modules/linux-ubuntu20.04-x86_64:/modules/modulefiles"
 
-#echo "spack install $SPACK_PACKAGE_NAME"
-#[[ "$(read -e -p 'is this correct? [y/N]> '; echo $REPLY)" == [Yy]* ]] || exit
+SPACK_INSTALL_ARGS=$@
+JOB_NAME="${SPACK_INSTALL_ARGS// /_}" # find and replace spaces with underscores
+START_DT=$(date +"%Y-%m-%d-%T")
 
-
-IFS=$'\n' read -d '' -r -a lines < state/archlist.txt
-
-for i in "${lines[@]}"
-do
-    echo "Queuing job for architecture $i..."
-    sbatch --job-name="$JOB_NAME" --constraint=$i --output=logs/$JOB_NAME-$i.out \
-	    --export=SPACK_PACKAGE_NAME="$SPACK_PACKAGE_NAME" slurm/slurm-install-batch.sh
-    echo log file: logs/$JOB_NAME-$i.out
+NUM_JOBS=0
+for arch in $(<state/archlist.txt); do
+    LOG_FILE="logs/${JOB_NAME}_${arch}_${START_DT}.out"
+    echo "install # $(( $NUM_JOBS+1 )): Queuing job for architecture $arch..."
+    echo $LOG_FILE
+    sbatch --wait --job-name="build_$JOB_NAME" --constraint=$arch --output=$LOG_FILE \
+            --export=SPACK_INSTALL_ARGS="$SPACK_INSTALL_ARGS" slurm/slurm-install-batch.sh\
+            & # & means run this in the background
+    ((NUM_JOBS++))
 done
 
-# TODO use -d to make another job dependent on all these which can let me know when they are done
-# add to package file if not already there
-# TODO only if jobs were a success
-grep -qxF "$SPACK_PACKAGE_NAME" state/packagelist.txt || echo $SPACK_PACKAGE_NAME >> state/packagelist.txt
+echo "this might take a while. You can break out of this script and the installs will continue,"
+echo "but you will have to check by hand that the installs were successful."
 
-# TODO never use spack gc it's broken, use Simon's Lmod hiding script and then Simon's lmod_regen_modfiles alias
-# finished submitting jobs
-#echo "Remember to run spack gc afterwards to remove excess build dependencies"
+ANY_FAILURES=0
+for ((i=1; i<($NUM_JOBS+1); i++)); do
+    # wait %i -> get the return code for background process i
+    # multiple waits called in sequence will run in parallel
+    # the background processes are indexed starting at 1
+    wait %$i
+    if [ ! $? -eq 0 ]; then
+        echo "install # $i has failed!"
+        ANY_FAILURES=1
+    fi
+done
+
+if [ $ANY_FAILURES -eq 1 ]; then
+    exit 1
+fi
+
+grep -qxF "$SPACK_INSTALL_ARGS" state/packagelist.txt || echo $SPACK_INSTALL_ARGS >> state/packagelist.txt
+echo "regenerating Lmod spider cache using hard-coded modulepath. This may change someday!"
+echo $HARD_MODULEPATH
+# TODO hide implicit modules
+/modules/lmod/lmod/lmod/libexec/update_lmod_system_cache_files -d /modules/lmod/cache $HARD_MODULEPATH
