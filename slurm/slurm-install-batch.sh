943a5d97e8eaa83779a2222d307a1df02c503432
simon <simon.leary42@gmail.com>
2022-08-25 12:40:32 -0400

    specify arch

--- slurm/slurm-install-batch.sh
+++ slurm/slurm-install-batch.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo jobid $SLURM_JOB_ID on host $(hostname) with arch $(/modules/spack/bin/spack arch) by user $(whoami) on $(date)
+ARCH=$(/modules/spack/bin/spack arch)
+echo jobid $SLURM_JOB_ID on host $(hostname) with arch $ARCH by user $(whoami) on $(date)
+
 
 source /etc/profile # refresh $MODULEPATH
 echo "Loading spack environment..."
 source /modules/spack/share/spack/setup-env.sh
 
-echo spack install --verbose -y $SPACK_INSTALL_ARGS
-spack install --verbose -y $SPACK_INSTALL_ARGS
+echo spack install --verbose -y $SPACK_INSTALL_ARGS arch=$ARCH
+spack install --verbose -y $SPACK_INSTALL_ARGS arch=$ARCH
 
 #--keep-stage will leave the build logs in /scratch, which will get cleaned up by /scratch anyways
 # but it only does so if the installation was a success, and that's exactly when I don't want the logs
