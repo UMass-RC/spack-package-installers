65687415e6525c4d2d2c20b61ddc28c93c33c0e7
simon <simon.leary42@gmail.com>
2022-08-25 15:29:45 -0400

    add no wait feature

--- install-all-packages.sh
+++ install-all-packages.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-while read p; do
-  ./install-package $p
-  sleep 1
+while getopts "a:" option; do
+    case $option in
+        a) USER_ARCH=$OPTARG;;
+    esac
+done
+
+packages=$(<state/archlist.txt)
+
+for package in $packages; do
+    # if $USER_ARCH is defined
+    if [ ! -z ${USER_ARCH+x} ]; then
+        ./install-package -w -a $USER_ARCH $package
+    else
+        ./install-package -w $package
+    sleep 1
 done < installed_packages
--- install-package.sh
+++ install-package.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
+# this is a script that starts batch jobs and --wait's for them, and then cleans up after them.
+
 # USAGE
-# ./install-package [-a architecture] [-f]  [-g] <spack package spec>
+# ./install-package [-a architecture] [-c] [-f]  [-g] <spack package spec>
 # -a install for a specific architecture rather than read from state/archlist.txt
+# -c skip the post install cleanup
 # -f spack install --fresh
 # -g get a GPU for the job
+# -w don't wait for the jobs to finish
 
 # you can also export EXTRA_SPACK_ARGS and they will be inserted
 # you can also export EXTRA_SBATCH_ARGS and they will be inserted
 # if you want to export variables to this script, start another nested shell,
 # because `exit` will kill your shell
 
-while getopts "a:fg" option; do
+NO_CLEANUP=0
+DO_WAIT="--wait"
+while getopts "a:cdfgw" option; do
     case $option in
         a) USER_ARCH=$OPTARG;;
+        c) NO_CLEANUP=1;;
         f) EXTRA_SPACK_ARGS="$EXTRA_SPACK_ARGS --fresh";; # todo no duplicates
         g) EXTRA_SBATCH_ARGS="$EXTRA_SBATCH_ARGS -G 1";; # todo no duplicates
+        w) DO_WAIT="";;
     esac
 done
 shift $(($OPTIND - 1)) # remove processed args from $@
-ALL_ARGS=$@
-ALL_ARGS=$(echo $ALL_ARGS | sed 's/ *$//g') # strip trailing whitespace
-ALL_ARGS=$(echo $ALL_ARGS | sed 's/^ *//g') # strip leading whitespace
 
 NUM_CORES=8
 TIME="1-0"
 PARTITION="building"
 
-SPACK_INSTALL_ARGS=$ALL_ARGS
+SPACK_INSTALL_ARGS=$@
 # if EXTRA_SPACK_ARGS is defined, prepend it to SPACK_INSTALL_ARGS
 if [ ! -z ${EXTRA_SPACK_ARGS+x} ]; then
     SPACK_INSTALL_ARGS="$EXTRA_SPACK_ARGS $SPACK_INSTALL_ARGS"
@@ -35,7 +40,11 @@ fi
 
 JOB_NAME="${SPACK_INSTALL_ARGS//-}" # remove dashes
 JOB_NAME="${JOB_NAME// /_}" # find and replace spaces with underscores
-RANDOM_STR=$( echo $RANDOM | md5sum | head -c 5; echo;)
+
+# if RANDOM_STR is undefined, define it
+if [ -z ${RANDOM_STR+x} ]; then
+    RANDOM_STR=$( echo $RANDOM | md5sum | head -c 5; echo;)
+fi
 
 echo "Loading spack environment..."
 source /modules/spack/share/spack/setup-env.sh
@@ -51,20 +60,26 @@ for arch in $arches; do
     LOG_FILE="logs/${JOB_NAME}_${arch}_${RANDOM_STR}.out" # random so that logs don't overwrite
     echo "install #$(( $NUM_JOBS+1 ))"
     echo "$LOG_FILE"
-    echo sbatch --wait --job-name="build_$JOB_NAME" --constraint=$arch --output=$LOG_FILE \
+    echo sbatch $DO_WAIT --job-name="build_$JOB_NAME" --constraint=$arch --output=$LOG_FILE \
             ${EXTRA_SBATCH_ARGS} --partition=$PARTITION --cpus-per-task=$NUM_CORES --time=$TIME\
             --export=SPACK_INSTALL_ARGS="$SPACK_INSTALL_ARGS" slurm/slurm-install-batch.sh
-    sbatch --wait --job-name="build_$JOB_NAME" --constraint=$arch --output=$LOG_FILE \
+    sbatch $DO_WAIT --job-name="build_$JOB_NAME" --constraint=$arch --output=$LOG_FILE \
             ${EXTRA_SBATCH_ARGS} --partition=$PARTITION --cpus-per-task=$NUM_CORES --time=$TIME\
             --export=SPACK_INSTALL_ARGS="$SPACK_INSTALL_ARGS" slurm/slurm-install-batch.sh &
             # & means run this in the background
     ((NUM_JOBS++))
 done
 
+# if we're not waiting
+if [ ! $DO_WAIT -eq "--wait" ]; then
+    exit
+fi
+
 echo
 echo "this might take a while. You can break out of this script and the installs will continue,"
 echo "but you will have to manually check the installs were successful, and do the post install cleanup."
 echo "use tmux to detach a session and let it run in the background."
+echo "you can disable this waiting using the `-w` flag."
 echo
 
 ANY_FAILURES=0
@@ -93,4 +108,6 @@ if [ $ANY_FAILURES -eq 1 ]; then
     exit 1
 fi
 
-./post-install-cleanup.sh
+if [ NO_CLEANUP == 0 ]; then
+    ./post-install-cleanup.sh
+fi
